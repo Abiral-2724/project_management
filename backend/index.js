fb7249d852f9cf0715bacc77060c2e3788dc0f08
Abiral-2724 <abiral2004jain@gmail.com>
2026-03-06 00:12:08 +0530

    frontend url updated at backend

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "https://project-management-gold-phi.vercel.app" || "http://localhost:3000" ,
     credentials: true,
   })
 );
